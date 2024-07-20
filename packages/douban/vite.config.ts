d7728cc59ad272e0648db3ee0d2f02b34ce414ff
Leo <xuebin.liu@zhaopin.com.cn>
2024-07-20 20:03:23 +0800

    chore: add douban project

---
+++ packages/douban/vite.config.ts
@@ -0,0 +1,39 @@
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import monkey, { cdn } from 'vite-plugin-monkey'
+import packageInfo from './package.json'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // server: {
+  //   origin: 'http://127.0.0.1:5173',
+  // },
+  build: {
+    outDir: '../../',
+  },
+  plugins: [
+    vue(),
+    monkey({
+      entry: 'src/main.ts',
+      userscript: {
+        icon: 'https://vitejs.dev/logo.svg',
+        namespace: 'https://github.com/MrLeo/userscript',
+        homepage: 'https://xuebin.me',
+        author: 'https://xuebin.me',
+        match: ['*://wiki.zhaopin.com/*'],
+        updateURL: `https://gitee.com/mr.leo/userscript/raw/main/${packageInfo.name}.user.js`,
+        downloadURL: `https://gitee.com/mr.leo/userscript/raw/main/${packageInfo.name}.user.js`,
+        require: [
+          'https://cdn.jsdelivr.net/npm/core-js-bundle@latest/minified.js',
+          'https://cdn.jsdelivr.net/npm/table-to-json@1.0.0/lib/jquery.tabletojson.min.js',
+        ],
+      },
+      build: {
+        externalGlobals: {
+          jQuery: cdn.jsdelivr('jquery', 'dist/jquery.min.js'),
+          vue: cdn.jsdelivr('Vue', 'dist/vue.global.prod.js'),
+        },
+      },
+    }),
+  ],
+})
